d7e792fa5c10adf55f3602b6550a25d73fee5ae8
say-102 <say70007@naver.com>
2020-10-25 16:03:59 +0900

    입력한 데이터를 들고 => 결과로 설정해서 => 메인으로 복귀

--- app/src/main/java/kr/co/tjoeun/intent_20201025/EditDataActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20201025/EditDataActivity.kt
@@ -1,11 +1,28 @@
 package kr.co.tjoeun.intent_20201025
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_data.*
 
 class EditDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_data)
+
+        okBtn.setOnClickListener {
+
+            val inputData = newDataEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("newData", inputData)
+
+            setResult(Activity.RESULT_OK, resultIntent)
+
+            finish()h
+
+        }
+
     }
 }
