d4485a09267bd1694cbf525e9a804644327326a1
say-102 <say70007@naver.com>
2020-10-25 14:50:01 +0900

    finish()를 활용해서 이전화면으로 돌아가기

--- app/src/main/java/kr/co/tjoeun/intent_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20201025/MainActivity.kt
@@ -1,11 +1,24 @@
 package kr.co.tjoeun.intent_20201025
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherActivityBtn.setOnClickListener {
+
+//            다른 화면 (마이퍼스트엑티비티) 으로 이동
+
+            val myIntent = Intent(this, MyFirstActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
--- app/src/main/java/kr/co/tjoeun/intent_20201025/MyFirstActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20201025/MyFirstActivity.kt
@@ -1,11 +1,26 @@
 package kr.co.tjoeun.intent_20201025
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_my_first.*
 
 class MyFirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_first)
+
+        returnToMainBtn.setOnClickListener {
+
+//            현재 화면을 종료하고 이전으로 돌아가자.
+//            Intent 쓰는것 대신 => finish() 처리
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+
+        }
+
     }
 }
